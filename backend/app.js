0cbfd1538078dfadc9612b7914f9353bc74b9329
Cristian <cristhianmosquera723@gmail.com>
2025-06-26 19:02:37 -0500

    Primer commit GestionMax

---
+++ backend/app.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const path = require('path');
+require('dotenv').config();
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+mongoose.connect(process.env.MONGO_URI || '', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+})
+.then(() => console.log('🟢 Conectado a MongoDB'))
+.catch((err) => console.error('🔴 Error en MongoDB:', err));
+
+// Ruta raíz
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public/Menu_principal.html'));
+});
